c85995dbb1f1a3c618cd1109d38bc9d82e6c516f
ftmnkrb <fkarabacak026@gmail.com>
2023-07-25 17:52:44 +0300

    cevap seçenekleri kontrolü, soru sayısının gösterilmesi

--- script.js
+++ script.js
@@ -1,67 +1,33 @@
 // OOP : Nesne Tabanlı Programlama
 
 
-
-function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
-     this.soruMetni = soruMetni;
-     this.cevapSecenekleri = cevapSecenekleri;
-     this.dogruCevap = dogruCevap;
-     
-
-
-}
-
-Soru.prototype.cevabiKontrolEt = function(cevap) {
-     return cevap == this.dogruCevap;
-}
-
-let soru1 = new Soru("Hangisi .net paket yönetim uygulamasıdır?", { a : "Node.js", b : "nuget",c : "Npm"}, "b");
-
-let soru2 = new Soru("Hangisi Javascript paket yönetim uygulamasıdır?", { a : "Node.js", b : "Typescript",c : "Npm"}, "c");
-
-
-
-let sorular = [   // burada bir dizi içinde oluşturduk
-     new Soru("1-Yazılım nedir?", { a : "Program", b : "Veri",c : "İşletim", d : "Kod"}, "a"),
-     new Soru("2-Yazılım geliştirme sürecindeki adımlar nelerdir?", { a : "Tasarım", b : "Test",c : "Dağıtım"}, "b"),
-     new Soru("3-Nesne tabanlı programlama nedir?", { a : "Paradigma", b : "Yaklaşım",c : "Dil"}, "a"),
-     new Soru("4-Yazılım test etmenin amacı nedir?", { a : "Hata", b : "Doğruluk",c : "Kalite"}, "a"),
-]
-
-function Quiz(sorular){
-     this.sorular = sorular;
-     this.soruIndex=0 ;
-}
-
-// console.log(new Quiz(sorular[1]));
-
-
-
-Quiz.prototype.soruGetir = function() {
-     return this.sorular[this.soruIndex];
-}
 const quiz = new Quiz(sorular);
+const ui = new UI();
 
 
 
 
-document.querySelector(".btn_start").addEventListener("click", function() {
-          document.querySelector(".quiz_box").classList.add("active");
+
+ui.btn_start.addEventListener("click", function() {
+          ui.quiz_box.classList.add("active");
    
-          soruGoster(quiz.soruGetir());
-          document.querySelector(".next_btn").classList.remove("show");
+          ui.soruGoster(quiz.soruGetir());
+          ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+          ui.btn_next.classList.remove("show");
          
      
 
 
 })
 
-document.querySelector(".next_btn").addEventListener("click", function() {
+ui.btn_next.addEventListener("click", function() {
      if(quiz.sorular.length != quiz.soruIndex + 1 ){
           
           quiz.soruIndex += 1;
-          soruGoster(quiz.soruGetir());
-          document.querySelector(".next_btn").classList.remove("show");
+          ui.soruGoster(quiz.soruGetir());
+          ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+
+          ui.btn_next.classList.remove("show");
 
      }
      else{
@@ -72,33 +38,12 @@ document.querySelector(".next_btn").addEventListener("click", function() {
 const option_list = document.querySelector(".option_list")
 
 
-function soruGoster(soru) {
-     let question = `<span>${soru.soruMetni}</span>`;
-     let options = '';
 
-     for(let cevap in soru.cevapSecenekleri){
-          options += `
-          <div class="option">
-               <span><b>${cevap}</b> : ${soru.cevapSecenekleri[cevap]}</span>
-           </div>`;
-         
 
-     }
 
-     // const option_list = document.querySelector(".option_list")
-     document.querySelector(".question_text").innerHTML = question;
-     option_list.innerHTML = options;
 
-     const option = option_list.querySelectorAll(".option");
-
-     for(let opt of option){
-          opt.setAttribute("onclick", "optionSelected(this)")
-     }
-
-}
-
-const correctIcon = '<div class="icon"><i class="fa-solid fa-check"></i></div>'
-const incorrectIcon = '<div class="icon"><i class="fa-regular fa-circle-xmark"></i></div>'
+// const correctIcon = '<div class="icon"><i class="fa-solid fa-check"></i></div>'
+// const incorrectIcon = '<div class="icon"><i class="fa-regular fa-circle-xmark"></i></div>'
 
 function optionSelected(option){
      let cevap = option.querySelector("span b").textContent;
@@ -106,22 +51,24 @@ function optionSelected(option){
 
      if(soru.cevabiKontrolEt(cevap)){
           option.classList.add("correct");
-          option.insertAdjacentHTML("beforeend", correctIcon);
+          option.insertAdjacentHTML("beforeend", ui.correctIcon);
      }
      else{
           option.classList.add("incorrect");
-          option.insertAdjacentHTML("beforeend", incorrectIcon);
+          option.insertAdjacentHTML("beforeend", ui.incorrectIcon);
 
 
      }
 
-     for(let i=0; i< option_list.children.length; i++) {
-          option_list.children[i].classList.add("disabled");
+     for(let i=0; i< ui.option_list.children.length; i++) {
+          ui.option_list.children[i].classList.add("disabled");
      }
 
-     document.querySelector(".next_btn").classList.add("show");
+     ui.btn_next.classList.add("show");
 
      
      }
+
+
 
 
