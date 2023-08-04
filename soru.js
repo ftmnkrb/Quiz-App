2ac534c9fac474c3f95a7bd86dfa7dbb8aa57359
ftmnkrb <fkarabacak026@gmail.com>
2023-08-04 10:26:21 +0300

    sorular değiştirildi

--- soru.js
+++ soru.js
@@ -14,7 +14,7 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 
 let sorular = [   // burada bir dizi içinde oluşturduk
      new Soru("1-Yazılım nedir?", { a : "Program", b : "Veri",c : "İşletim", d : "Kod"}, "a"),
-     new Soru("2-Yazılım geliştirme sürecindeki adımlar nelerdir?", { a : "Tasarım", b : "Test",c : "Dağıtım"}, "b"),
-     new Soru("3-Nesne tabanlı programlama nedir?", { a : "Paradigma", b : "Yaklaşım",c : "Dil"}, "a"),
-     new Soru("4-Yazılım test etmenin amacı nedir?", { a : "Hata", b : "Doğruluk",c : "Kalite"}, "a"),
+     new Soru("2-Hangisi backend kapsamında değerlendirilir?", { a : "node.js", b : "typescript",c : "angular",d : "react"}, "a"),
+     new Soru("3-Hangisi frontend kapsamında değerlendirilmez?", { a : "CSS", b : "HTML",c : "JavaScript", d : "Sql"}, "d"),
+     new Soru("4-Hangisi JS programlama dilini kullanmaz?", { a : "React", b : "Angular",c : "Vue.js", d : "Asp.net"}, "d"),
 ]
